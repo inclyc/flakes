c5f93ecdfca338fa1b97e76dabb3b4fa68592538
Yingchi Long <i@lyc.dev>
2023-01-09 10:52:24 +0800

    [zsh] add p10k.zsh & zshrc

--- modules/app/shells/zsh/zsh.nix
+++ modules/app/shells/zsh/zsh.nix
@@ -1,32 +1,37 @@
 { pkgs, lib, config, ... }:
 {
-    programs.zsh = {
-      enable = true;
-      shellAliases = {
-        ls = "ls --color=auto";
-        ll = "ls -l";
-        update = "sudo nixos-rebuild switch --flake";
-        ip = "ip --color=auto";
-        grep = "grep --color=auto";
-      };
-
-      enableAutosuggestions = true;
-      enableCompletion = true;
-      enableSyntaxHighlighting = true;
-      historySubstringSearch.enable = true;
-      autocd = true;
-      dotDir = ".config/zsh";
-      defaultKeymap = "viins";
-      completionInit = "autoload -Uz compinit && compinit -iCd ${config.xdg.cacheHome}/zcompdump-$ZSH_VERSION";
-      history.path = "${config.xdg.dataHome}/zsh/zsh_history";
-
-      zplug = {
-        enable = true;
-        zplugHome = "${config.xdg.dataHome}/zplug";
-        plugins = [
-          { name = "hlissner/zsh-autopair"; }
-          { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
-        ];
-      };
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ls = "ls --color=auto";
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch --flake";
+      ip = "ip --color=auto";
+      grep = "grep --color=auto";
     };
+
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+    historySubstringSearch.enable = true;
+    autocd = true;
+    dotDir = ".config/zsh";
+    defaultKeymap = "viins";
+    completionInit = "autoload -Uz compinit && compinit -iCd ${config.xdg.cacheHome}/zcompdump-$ZSH_VERSION";
+    history.path = "${config.xdg.dataHome}/zsh/zsh_history";
+
+    zplug = {
+      enable = true;
+      zplugHome = "${config.xdg.dataHome}/zplug";
+      plugins = [
+        { name = "hlissner/zsh-autopair"; }
+        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+      ];
+    };
+
+    initExtra = ''
+      source ${./zshrc}
+      source ${./p10k.zsh}
+    '';
+  };
 }
