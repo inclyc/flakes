c64025ac05d6de5f01a6758acdf19c442866802b
Yingchi Long <i@lyc.dev>
2023-01-10 10:17:38 +0800

    [flake] add NUR

--- flake.nix
+++ flake.nix
@@ -7,9 +7,10 @@
     };
 
     flake-utils.url = "github:numtide/flake-utils";
+    nur.url = github:nix-community/NUR;
   };
 
-  outputs = { self, nixpkgs , ... }@inputs: {
+  outputs = { self, nixpkgs, nur, ... }@inputs: {
 
     nixosConfigurations = {
       lyc-desktop = import ./nixos/lyc-desktop { system = "x86_64-linux"; inherit self nixpkgs inputs; };
