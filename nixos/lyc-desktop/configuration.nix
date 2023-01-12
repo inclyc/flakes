398a935bb57f8f7fc1a3b9f45a84415ceaa07d64
Yingchi Long <i@lyc.dev>
2023-01-12 14:15:28 +0800

    Revert "desktop: set sessionVariable to hint electron apps use wayland"
    This reverts commit ff2393cfa5d47e89ca06ffead91d6bb721dd654e. VSCode in
    
    wayland breaks fcitx5.

--- nixos/lyc-desktop/configuration.nix
+++ nixos/lyc-desktop/configuration.nix
@@ -164,8 +164,6 @@
   environment.shells = with pkgs; [ zsh ];
   environment.localBinInPath = true;
 
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
