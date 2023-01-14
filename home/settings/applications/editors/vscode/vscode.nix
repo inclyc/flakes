79cb5bdf7567e65958bf4ef069175a9596f67f3c
Yingchi Long <i@lyc.dev>
2023-01-14 23:55:43 +0800

    home: new hone configuration "inclyc@amalthea"
    Host: MacBook Air "Amalthea", aarch64-darwin

--- home/settings/applications/editors/vscode/vscode.nix
+++ home/settings/applications/editors/vscode/vscode.nix
@@ -1,9 +1,9 @@
 { pkgs, lib, config, ... }:
 {
   programs.vscode = {
-    enable = true;
-    userSettings = builtins.fromJSON (builtins.readFile ./settings.json);
-    extensions = with pkgs.vscode-extensions; [
+    enable = lib.mkDefault true;
+    userSettings = lib.mkDefault (builtins.fromJSON (builtins.readFile ./settings.json));
+    extensions = lib.mkDefault (with pkgs.vscode-extensions; [
       vadimcn.vscode-lldb
       llvm-vs-code-extensions.vscode-clangd
       jnoortheen.nix-ide
@@ -14,6 +14,6 @@
       usernamehw.errorlens
       james-yu.latex-workshop
       shardulm94.trailing-spaces
-    ];
+    ]);
   };
 }
