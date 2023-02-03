67ee8ced13a7756ec85252fd89a8e9ae81d954e4
Yingchi Long <i@lyc.dev>
2023-02-03 09:14:22 +0800

    [home] split vscode modules

--- home/lyc/common/applications/editors/vscode/vscode.nix
+++ home/lyc/common/applications/editors/vscode/vscode.nix
@@ -3,8 +3,7 @@
   programs.vscode = {
     enable = lib.mkDefault true;
     userSettings = (builtins.fromJSON (builtins.readFile ./settings.json));
-    extensions = lib.mkDefault (with pkgs.vscode-extensions; [
-      vadimcn.vscode-lldb
+    extensions = with pkgs.vscode-extensions; [
       llvm-vs-code-extensions.vscode-clangd
       jnoortheen.nix-ide
       eamodio.gitlens
@@ -14,13 +13,8 @@
       usernamehw.errorlens
       james-yu.latex-workshop
       shardulm94.trailing-spaces
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "llvm-syntax-highlighting";
-        publisher = "colejcummins";
-        version = "0.0.3";
-        sha256 = "sha256-D5zLp3ruq0F9UFT9emgOBDLr1tya2Vw52VvCc40TtV0=";
-      }
-    ]);
+      colejcummins.llvm-syntax-highlighting
+      ms-vscode-remote.remote-ssh
+    ];
   };
 }
