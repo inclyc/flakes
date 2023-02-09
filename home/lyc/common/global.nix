bce3f7e48434b808a4bf91ac8481241323c8e93d
Yingchi Long <i@lyc.dev>
2023-02-10 02:28:43 +0800

    [nix] sync home-manager and nixos "nix" settings

--- home/lyc/common/global.nix
+++ home/lyc/common/global.nix
@@ -11,6 +11,15 @@
   nix = {
     package = lib.mkDefault pkgs.nix;
     settings = {
+      substituters = [
+        "https://mirrors.bfsu.edu.cn/nix-channels/store"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-users = [ "root" "@wheel" ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      auto-optimise-store = lib.mkDefault true;
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
       warn-dirty = false;
     };
--- nixos/common/global/nix.nix
+++ nixos/common/global/nix.nix
@@ -4,10 +4,12 @@
     settings = {
       substituters = [
         "https://mirrors.bfsu.edu.cn/nix-channels/store"
-        "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
       ];
       trusted-users = [ "root" "@wheel" ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
       auto-optimise-store = lib.mkDefault true;
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
       warn-dirty = false;
