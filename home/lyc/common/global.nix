24145b2757e4f9a5f503db5467bf79de5bebc07e
Yingchi Long <i@lyc.dev>
2023-02-10 22:25:10 +0800

    [home][nix] trust cache.nixos.org public key

--- home/lyc/common/global.nix
+++ home/lyc/common/global.nix
@@ -18,6 +18,7 @@
       trusted-users = [ "root" "@wheel" ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
       auto-optimise-store = lib.mkDefault true;
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
