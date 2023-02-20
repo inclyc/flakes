ddb34b326c168c6aca1d512cbdf013fd9ef38a99
Yingchi Long <i@lyc.dev>
2023-02-20 08:54:55 +0800

    [home][amalthea] remove vscode extension settings

--- home/lyc/amalthea/default.nix
+++ home/lyc/amalthea/default.nix
@@ -8,18 +8,5 @@
   home.homeDirectory = "/Users/${config.home.username}";
   home.username = "inclyc";
 
-  programs.vscode.extensions = (with pkgs.vscode-extensions; [
-    llvm-vs-code-extensions.vscode-clangd
-    jnoortheen.nix-ide
-    eamodio.gitlens
-    mhutchie.git-graph
-    asvetliakov.vscode-neovim
-    arcticicestudio.nord-visual-studio-code
-    usernamehw.errorlens
-    james-yu.latex-workshop
-    shardulm94.trailing-spaces
-
-  ]);
-
   programs.vscode.userSettings = (builtins.fromJSON (builtins.readFile ./vscode-settings.json));
 }
