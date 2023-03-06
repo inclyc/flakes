24491c28270ac8b3ece0e03763f64359c04e4999
Yingchi Long <i@lyc.dev>
2023-03-07 02:10:08 +0800

    nixos/adrastea: set boot.tmpOnTmpfs

--- nixos/adrastea/default.nix
+++ nixos/adrastea/default.nix
@@ -166,6 +166,8 @@
   environment.shells = with pkgs; [ zsh ];
   environment.localBinInPath = true;
 
+  boot.tmpOnTmpfs = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
