64ac704415ed9c1dba58139f17ef167e7408282b
Yingchi Long <i@lyc.dev>
2023-04-30 02:32:48 +0800

    nixos: do not set pam loginLimits by default, this prevent metis login

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -23,23 +23,6 @@
     ];
   };
 
-  # Increase open file limit for sudoers
-  security.pam.loginLimits = [
-    {
-      domain = "@wheel";
-      item = "nofile";
-      type = "soft";
-      value = "524288";
-    }
-    {
-      domain = "@wheel";
-      item = "nofile";
-      type = "hard";
-      value = "1048576";
-    }
-  ];
-
-
   environment.systemPackages = with pkgs; [
     git
     home-manager
