2badc89ac8120c8fe06267407aced41120365737
Yingchi Long <i@lyc.dev>
2023-04-30 15:26:05 +0800

    devShells/llvm: add workaround for clangd wrapper search path ($PATH)

--- devShells/llvm/default.nix
+++ devShells/llvm/default.nix
@@ -14,5 +14,12 @@ with pkgs; pkgs.mkShell.override { inherit stdenv; }
 
     # for "lit" testing
     python3
+
+    clang-tools
   ];
+
+  # workaround for https://github.com/NixOS/nixpkgs/issues/76486
+  shellHook = ''
+     PATH="${clang-tools}/bin:$PATH"
+  '';
 }
