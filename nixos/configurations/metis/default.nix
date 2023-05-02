2c5cec21b933db2b64cfd9fa239007274c1e6771
Yingchi Long <i@lyc.dev>
2023-05-02 16:21:01 +0800

    nixos/metis: use static networking

--- nixos/configurations/metis/default.nix
+++ nixos/configurations/metis/default.nix
@@ -12,6 +12,14 @@
 
   networking.hostName = "metis";
 
+  networking.interfaces.eth0.ipv4.addresses = [{
+    address = "192.168.31.6";
+    prefixLength = 24;
+  }];
+
+  networking.defaultGateway = "192.168.31.1";
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+
   # Set your time zone.
   time.timeZone = "Asia/Hong_Kong";
 
