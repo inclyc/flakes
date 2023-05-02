ef9b19f363b165ca411c414221354aeaae7127a5
Yingchi Long <i@lyc.dev>
2023-05-02 17:11:21 +0800

    nixos/dione: init

---
+++ nixos/configurations/dione/default.nix
@@ -0,0 +1,50 @@
+{ modulesPath
+, lib
+, inputs
+, config
+, ...
+}:
+{
+
+  imports = [
+    ("${modulesPath}" + "/virtualisation/qemu-vm.nix")
+  ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "riscv64-linux";
+  nixpkgs.buildPlatform = "x86_64-linux";
+
+
+  inclyc.user.enable = true;
+
+  services.getty.autologinUser = "root";
+
+  virtualisation = {
+    host.pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
+    qemu.package = config.virtualisation.host.pkgs.qemu;
+    cores = 16;
+    memorySize = 4096;
+    graphics = false;
+  };
+
+  networking.hostName = "dione";
+
+  # Set your time zone.
+  time.timeZone = "Asia/Hong_Kong";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      passwordAuthentication = false;
+      kbdInteractiveAuthentication = false;
+    };
+  };
+
+  networking.firewall.enable = false;
+
+  system.stateVersion = "22.11";
+
+  programs.nix-ld.enable = lib.mkForce false;
+}
