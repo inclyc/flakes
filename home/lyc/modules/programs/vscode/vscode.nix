e048a5c7dce2ab1056cd2450bf166e5b83222263
Yingchi Long <i@lyc.dev>
2023-05-05 08:36:26 +0800

    home/vscode: typo fix (NFC)

--- home/lyc/modules/programs/vscode/vscode.nix
+++ home/lyc/modules/programs/vscode/vscode.nix
@@ -16,7 +16,7 @@
         {
           # path.linux may specified "incorrectly" on darwin
           # because it will link to *darwin* executable. This configuration
-          # should be evaulated on corresponding platform, and choosed by vscode.
+          # should be evaulated on corresponding platform, and chosed by vscode.
           "vscode-neovim.neovimExecutablePaths.linux" = nvimPath;
           "vscode-neovim.neovimExecutablePaths.darwin" = nvimPath;
 
