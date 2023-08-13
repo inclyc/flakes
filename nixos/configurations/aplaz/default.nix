ba8e2db949b7de4665e276d7372cff9fc9324249
Yingchi Long <i@lyc.dev>
2023-08-13 19:59:59 +0800

    nixos/aplaz: set `vm.swappiness=100`

--- nixos/configurations/aplaz/default.nix
+++ nixos/configurations/aplaz/default.nix
@@ -21,6 +21,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 100;
+  };
+
   networking.hostName = "aplaz"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
