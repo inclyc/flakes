4e49d35eb72aa54bc02e3ef7f10e3ee4f4c3bd09
Yingchi Long <i@lyc.dev>
2023-09-23 17:18:16 +0800

    home/zsh: move zsh syntax highlighting to the new name

--- home/lyc/modules/programs/zsh/zsh.nix
+++ home/lyc/modules/programs/zsh/zsh.nix
@@ -12,7 +12,7 @@
 
     enableAutosuggestions = true;
     enableCompletion = true;
-    enableSyntaxHighlighting = true;
+    syntaxHighlighting.enable = true;
     historySubstringSearch.enable = true;
     autocd = true;
     dotDir = ".config/zsh";
