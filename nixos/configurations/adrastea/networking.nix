2a646673082639246e4d12e3a842d3203b80b442
Yingchi Long <i@lyc.dev>
2023-09-27 18:29:45 +0800

    nixos/adrastea: use systemd-networkd networking

--- nixos/configurations/adrastea/networking.nix
+++ nixos/configurations/adrastea/networking.nix
@@ -1,34 +1,63 @@
 { lib, ... }:
 let
-  vtap = {
-    virtual = true;
-    virtualOwner = "lyc";
+  mkTapDev = name: {
+    "20-${name}" = {
+      netdevConfig = {
+        Kind = "tap";
+        Name = name;
+      };
+      tapConfig.User = "lyc";
+    };
+  };
+  mkBrSlave = name: br: {
+    "20-${name}" = {
+      matchConfig.Name = name;
+      bridge = [ br ];
+    };
   };
 in
 {
-  networking = {
-    networkmanager.enable = lib.mkForce false;
-    useDHCP = false;
+  networking.dhcpcd.enable = false;
 
-    interfaces.enp5s0.useDHCP = false;
-
-    # External bridge, connect to the internet directly
-    interfaces.brx0.useDHCP = true;
-    bridges.brx0.interfaces = [ "enp5s0" "vmtap0" ];
-
-    # NAT network for VMs
-    nat = {
-      enable = true;
-      externalInterface = "Meta";
-      internalIPs = [ "10.157.3.0/24" ];
+  systemd.network.enable = true;
+  systemd.network.networks = {
+    "20-br0" = {
+      matchConfig.Name = "br0";
+      networkConfig = {
+        DHCP = "ipv4";
+        DNS = "127.0.0.1:8053";
+        IPv6AcceptRA = true;
+      };
     };
-    bridges.bri0.interfaces = [ "vmtap1" ];
-    interfaces.bri0 = {
-      ipv4.addresses = [{ address = "10.157.3.1"; prefixLength = 24; }];
+    "20-br1" = {
+      matchConfig.Name = "br1";
+      networkConfig = {
+        DHCPServer = true;
+        IPMasquerade = "ipv4";
+      };
+      addresses = [
+        { addressConfig.Address = "10.157.0.1/24"; }
+      ];
     };
+  } // mkBrSlave "enp5s0" "br0"
+  // mkBrSlave "vmtap0" "br0"
+  // mkBrSlave "vmtap1" "br1";
 
-    # Pre-allocated interfaces for VM, create them
-    interfaces.vmtap0 = vtap;
-    interfaces.vmtap1 = vtap;
-  };
+  systemd.network.netdevs = {
+    "20-br0" = {
+      netdevConfig = {
+        Kind = "bridge";
+        Name = "br0";
+        MACAddress = "BE:CE:CA:F9:E8:D3";
+      };
+    };
+    "20-br1" = {
+      netdevConfig = {
+        Kind = "bridge";
+        Name = "br1";
+      };
+    };
+  } // (mkTapDev "vmtap0" // mkTapDev "vmtap1");
+
+  networking.nftables.enable = true;
 }
