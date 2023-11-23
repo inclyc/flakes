1c0214968d5e7f924fa03006acffbad9f957c1b3
Yingchi Long <i@lyc.dev>
2023-11-23 13:49:18 +0800

    nixos/clash: add more providers

--- nixos/modules/clash/rule.nix
+++ nixos/modules/clash/rule.nix
@@ -1,19 +1,28 @@
-{ config, ... }:
+{ lib, config, ... }:
+let
+  providers = [ "dler" "mielink" "bywave" ];
 
+  mkProvider = name: {
+    type = "http";
+    path = "./${name}.yaml";
+    url = "${config.sops.placeholder."clash-provider/${name}"}";
+    interval = 3600;
+    health-check = {
+      enable = true;
+      url = "http://www.gstatic.com/generate_204";
+      interval = 300;
+    };
+  };
+  mkSecret = name: {
+    "clash-provider/${name}" = { };
+  };
+
+  proxyProviders = lib.genAttrs providers mkProvider;
+in
 {
-  sops.secrets."clash-provider" = { };
-
+  sops.secrets = lib.attrsets.mergeAttrsList (map mkSecret providers);
   sops.templates."clash-config.yaml".content = builtins.readFile ./rule.yaml + ''
-    proxy-providers:
-     dlercloud:
-       type: http
-       path: ./dlercloud.yaml
-       url: "${config.sops.placeholder."clash-provider"}"
-       interval: 3600
-       health-check:
-         enable: true
-         url: http://www.gstatic.com/generate_204
-         interval: 300
+    proxy-providers: ${builtins.toJSON proxyProviders}
   '';
 
   sops.templates."clash-config.yaml".owner = config.services.clash.user;
