f6b9531bf344d9db88026e3b9e13e5522dbe477d
Yingchi Long <i@lyc.dev>
2023-12-12 16:10:48 +0800

    nixos/clash: generate proxy-groups.json, using nix

--- nixos/modules/clash/rule.nix
+++ nixos/modules/clash/rule.nix
@@ -18,10 +18,31 @@ let
   };
 
   proxyProviders = lib.genAttrs providers mkProvider;
+
+  proxyGroups = [
+    {
+      name = "Proxy";
+      type = "select";
+      use = providers;
+      proxies = [
+        "Auto"
+        "DIRECT"
+      ];
+    }
+    {
+      name = "Auto";
+      type = "url-test";
+      use = providers;
+      proxies = [ "DIRECT" ];
+      url = "http://cp.cloudflare.com/generate_204";
+      interval = "3600";
+    }
+  ] ++ builtins.fromJSON (builtins.readFile ./proxy-groups.json);
 in
 {
   sops.secrets = lib.attrsets.mergeAttrsList (map mkSecret providers);
   sops.templates."clash-config.yaml".content = builtins.readFile ./rule.yaml + ''
+    proxy-groups: ${builtins.toJSON proxyGroups}
     proxy-providers: ${builtins.toJSON proxyProviders}
   '';
 
