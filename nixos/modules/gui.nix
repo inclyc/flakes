67db019a624c4f65e00ef1d0625afaadf6056750
Yingchi Long <i@lyc.dev>
2024-04-01 14:12:47 +0800

    flake: update the lock

--- nixos/modules/gui.nix
+++ nixos/modules/gui.nix
@@ -84,7 +84,7 @@ in
 
     services.xserver.displayManager.sddm.enable = true;
     services.xserver.displayManager.sddm.wayland.enable = true;
-    services.xserver.desktopManager.plasma6.enable = true;
+    services.desktopManager.plasma6.enable = true;
 
     # Enable CUPS to print documents.
     services.printing.enable = true;
