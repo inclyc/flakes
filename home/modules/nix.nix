79d63be76a0855d9082fd0e7f9d0e80996aa70d6
Yingchi Long <i@lyc.dev>
2024-04-16 17:01:18 +0800

    home/modules/nix: remove trusted-users setting

--- home/modules/nix.nix
+++ home/modules/nix.nix
@@ -22,7 +22,6 @@
         "https://cache.nixos.org"
         "https://inclyc.cachix.org"
       ];
-      trusted-users = [ "root" "@wheel" ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
