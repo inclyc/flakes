c50a2f1cd5058eeefe04d082120851b019c6778e
Yingchi Long <i@lyc.dev>
2024-05-04 10:56:39 +0800

    nixos/aplaz: fixup escaping `with`

--- nixos/configurations/aplaz/default.nix
+++ nixos/configurations/aplaz/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ inputs, lib, pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 let
   proxy = "socks5://localhost:8899";
@@ -121,7 +121,7 @@ in
     rr
     ccache
     ninja
-    (lib.meta.hiPrio clang-tools_16)
+    (hiPrio clang-tools_16)
 
     python3
 
