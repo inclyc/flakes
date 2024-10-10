ae2ef55116759870582cdb13ac05e7c7510c6517
Yingchi Long <i@lyc.dev>
2024-10-10 22:46:27 +0800

    pkgs/wemeet: drop

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,6 +4,5 @@
 {
   topsap = pkgs.callPackage ./topsap { };
   code-oss = pkgs.callPackage ./code-oss/package.nix { };
-  wemeet = pkgs.callPackage ./wemeet/package.nix { };
   ddns = pkgs.python3.pkgs.callPackage ./ddns { };
 }
