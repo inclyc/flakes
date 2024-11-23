3be82138fbdb8db85c24189fac96815ff205bfc9
Yingchi Long <i@lyc.dev>
2024-11-23 21:22:16 +0800

    home/lyc/configurations/adrastea: avoid ~/.ssh/config permission error

--- home/lyc/configurations/adrastea/default.nix
+++ home/lyc/configurations/adrastea/default.nix
@@ -32,4 +32,9 @@
     user = "zxy";
     port = 22;
   };
+
+  home.file.".ssh/config" = {
+    target = ".ssh/config_source";
+    onChange = ''cat .ssh/config_source > .ssh/config && chmod 400 .ssh/config'';
+  };
 }
