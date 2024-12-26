8b18deb63afc4cbec7698a9284f094589fe15205
Yingchi Long <longyingchi24s@ict.ac.cn>
2024-12-26 17:33:52 +0800

    pkgs/{codium-reh -> vscodium-host}: add the web host

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,16 @@
 }:
 {
   topsap = pkgs.callPackage ./topsap { };
-  codium-reh = pkgs.callPackage ./codium-reh { };
   linux-fhs-python = pkgs.callPackage ./linux-fhs-python { };
   ddns = pkgs.python3.pkgs.callPackage ./ddns { };
+  vscodium-remote-host = pkgs.callPackage ./vscodium-host {
+    pname = "vscodium-remote-host";
+    assetName = "vscodium-reh";
+  };
+
+  vscodium-web-host = pkgs.callPackage ./vscodium-host {
+    pname = "vscodium-web-host";
+    assetName = "vscodium-reh-web";
+  };
+
 }
