f802ead9b0e7e8d7d9f9514ea5cc886b3aa6ac84
Yingchi Long <longyingchi24s@ict.ac.cn>
2025-01-05 02:30:29 +0800

    pkgs/vscode-host: init at 1.96.2

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,6 +5,9 @@
   topsap = pkgs.callPackage ./topsap { };
   linux-fhs-python = pkgs.callPackage ./linux-fhs-python { };
   ddns = pkgs.python3.pkgs.callPackage ./ddns { };
+
+  vscode-web-host = pkgs.callPackage ./vscode-host { };
+
   vscodium-remote-host = pkgs.callPackage ./vscodium-host {
     pname = "vscodium-remote-host";
     assetName = "vscodium-reh";
