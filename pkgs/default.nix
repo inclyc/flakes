b92968fc596856bc00392793284c918a9a4a455f
Yingchi Long <longyingchi24s@ict.ac.cn>
2025-04-06 18:29:26 +0800

    pkgs/linux-fhs-python: remove

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,6 @@
 }:
 {
   topsap = pkgs.callPackage ./topsap { };
-  linux-fhs-python = pkgs.callPackage ./linux-fhs-python { };
   ddns = pkgs.python3.pkgs.callPackage ./ddns { };
 
   simsun = pkgs.callPackage ./simsun { };
