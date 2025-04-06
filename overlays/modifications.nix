e31921576a668d2ce9b8a721301d301afc904374
Yingchi Long <longyingchi24s@ict.ac.cn>
2025-04-06 18:21:06 +0800

    overlays/openssh: remove permission checking (for FHS)
    In the previous version, a patch was used to disable the permission check for OpenSSH,
    mainly because this permission check was invalid in FHS.
    Now I primarily use an `LD_LIBRARY_PATH`-based method to bypass `FHS`-based solution,
    so this patch can be removed.

--- overlays/modifications.nix
+++ overlays/modifications.nix
@@ -4,7 +4,6 @@ let
 in
 (lib.composeManyExtensions [
   (import ./chromium.nix)
-  (import ./openssh)
   (import ./vscode)
 ])
   final
--- overlays/openssh/default.nix
+++
@@ -1,9 +0,0 @@
-/**
-  Disable "bad permission" checking in openssh.
-*/
-final: prev: {
-  openssh = prev.openssh.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [ ./no-check-permission.patch ];
-    doCheck = false;
-  });
-}
