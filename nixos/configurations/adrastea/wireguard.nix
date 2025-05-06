01271b7feffec46b61659be081475dd1814cb9f7
Yingchi Long <longyingchi24s@ict.ac.cn>
2025-05-06 23:06:20 +0800

    nixos/configurations/adrastea/wireguard: set 10.231.0.2, update info

--- nixos/configurations/adrastea/wireguard.nix
+++ nixos/configurations/adrastea/wireguard.nix
@@ -20,14 +20,8 @@
       wireguardPeers = [
         {
           PublicKey = "CMb1V4DaIsYRY5dNxZSzQZD6Xnd5/DNyDPJoRtosfgs=";
-          AllowedIPs = [ "10.231.0.1/16" ];
+          AllowedIPs = [ "10.231.0.0/16" ];
           Endpoint = "llvmws.inclyc.cn:20125";
-          PersistentKeepalive = 25;
-        }
-        {
-          PublicKey = "GcR4Wx7JUJOCO5Rs+gs0S4xbnC4NqU4ltCsSowjwEyg=";
-          AllowedIPs = [ "0.0.0.0/0" ];
-          Endpoint = "t.inclyc.cn:45229";
           PersistentKeepalive = 25;
         }
       ];
@@ -37,7 +31,7 @@
   systemd.network.networks = {
     wg0 = {
       matchConfig.Name = "wg0";
-      address = [ "10.164.2.8/16" ];
+      address = [ "10.231.0.2/16" ];
       DHCP = "no";
       networkConfig = {
         IPv6AcceptRA = false;
