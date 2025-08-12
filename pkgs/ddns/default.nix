f997f9900bc826b610fb1d162bf52f55b10839d9
Yingchi Long <longyingchi24s@ict.ac.cn>
2025-08-12 22:17:19 +0800

    pkgs/ddns: add pyproject and build-system

--- pkgs/ddns/default.nix
+++ pkgs/ddns/default.nix
@@ -1,8 +1,9 @@
-{ buildPythonApplication
-, fetchPypi
+{
+  buildPythonApplication,
+  fetchPypi,
+  setuptools,
 }:
-buildPythonApplication
-rec {
+buildPythonApplication rec {
   pname = "ddns";
   version = "2.12.0";
   name = "${pname}-${version}";
@@ -10,6 +11,9 @@ rec {
     inherit pname version;
     sha256 = "A41erAo7KOkgPpE+Izuof+MuUzOORPPraxFoj4/dtk0=";
   };
+
+  pyproject = true;
+  build-system = [ setuptools ];
 
   # Ungly hack for https://github.com/NewFuture/DDNS/blob/a1a8f8565c80d9702a49fcb45f18b32dfb4dba26/setup.py#L47
   # setup a fack "travis" environment, pass package version to skip the check
