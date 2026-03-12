3c90078f2331a1051289800fe12bcfab1e6b1055
Yingchi Long <longyingchi24s@ict.ac.cn>
2026-03-12 20:09:59 +0800

    home/lyc/modules/programs/zsh: add `PNPM_HOME`

--- home/lyc/modules/programs/zsh/zsh.nix
+++ home/lyc/modules/programs/zsh/zsh.nix
@@ -37,6 +37,11 @@
     initContent = ''
       source ${./zshrc}
       source ${./p10k.zsh}
+      export PNPM_HOME="$HOME/.local/share/pnpm"
+      case ":$PATH:" in
+        *":$PNPM_HOME:"*) ;;
+        *) export PATH="$PNPM_HOME:$PATH" ;;
+      esac
     '';
   };
 }
