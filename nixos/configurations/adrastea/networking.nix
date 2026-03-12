0b0ad7c4757cb3f4f370854ff2d95c191dc239fe
Yingchi Long <longyingchi24s@ict.ac.cn>
2026-03-12 19:19:30 +0800

    nixos/configurations/adrastea/networking: disable rathole

--- nixos/configurations/adrastea/networking.nix
+++ nixos/configurations/adrastea/networking.nix
@@ -28,26 +28,7 @@
 
   systemd.network.wait-online.anyInterface = true;
 
-  inclyc.services.rathole.enable = true;
-  sops.secrets."rathole-ssh-token" = { };
-  sops.templates."rathole-client.toml".content = ''
-    [client]
-    remote_addr = "llvmws.inclyc.cn:20155" # The address of the server. The port must be the same with the port in `server.bind_addr`
-
-    [client.transport]
-    type = "noise"
-
-    [client.transport.noise]
-    remote_public_key = "DVH4EM3P5phh5yzU7cOEDrDvdvahsiSSyeML+okHHx0="
-
-    [client.services.adrastea-ssh]
-    token = "${config.sops.placeholder.rathole-ssh-token}" # Must be the same with the server to pass the validation
-    local_addr = "0.0.0.0:22" # The address of the service that needs to be forwarded
-  '';
-
   services.tailscale.enable = true;
-
-  inclyc.services.rathole.configFile = config.sops.templates."rathole-client.toml".path;
 
   sops.secrets."ddns/cloudflare" = { };
   services.ddns."cloudflare-adrastea" = {
