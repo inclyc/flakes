df60865d473db16de6c9274bbfd86697cfbf7053
Yingchi Long <longyingchi24s@ict.ac.cn>
2026-03-25 11:44:36 +0800

    home/lyc/modules/programs/uv: use tuna mirror

--- home/lyc/modules/programs/uv.nix
+++ home/lyc/modules/programs/uv.nix
@@ -7,5 +7,11 @@ in
     # Fields reference: https://docs.astral.sh/uv/reference/settings
     python-downloads = "manual";
     python-preference = "managed";
+    index = [
+      {
+        url = "https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple";
+        default = true;
+      }
+    ];
   };
 }
