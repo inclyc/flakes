0a4a7fefc8f963402f6d814e3b8819b3db156b5a
Yingchi Long <longyingchi24s@ict.ac.cn>
2026-05-06 00:37:04 +0800

    nixos/configurations/adrastea/vm: start windows machine on boot

--- nixos/configurations/adrastea/vm/default.nix
+++ nixos/configurations/adrastea/vm/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 let
   add-vfio = pkgs.callPackage ./add-vfio { };
@@ -14,4 +14,23 @@ in
     # RDP client to connect Windows VM, faster than spice.
     freerdp
   ];
+
+  systemd.services."vm-windows" = {
+    description = "Windows VM Service";
+    after = [ "network.target" ];
+    wants = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      User = "lyc";
+      ExecStart = "${pkgs.python3}/bin/python3 vm_windows.py ./windows.qcow2";
+      WorkingDirectory = "${config.users.users.lyc.home}/ict-disks/虚拟机/";
+      Restart = "always";
+      RestartSec = 5;
+    };
+    path = with pkgs; [
+      qemu
+      python3
+    ];
+  };
 }
