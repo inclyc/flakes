e5080fb73eec38cbf0dc7e8fb0d5fda3e70408a3
Yingchi Long <longyingchi24s@ict.ac.cn>
2026-05-18 22:31:06 +0800

    nixos/configurations/metis/oparic: new backend (container-based)

--- nixos/configurations/metis/oparic/default.nix
+++ nixos/configurations/metis/oparic/default.nix
@@ -59,6 +59,22 @@ in
       "matomo.inclyc.cn" = {
         extraConfig = "reverse_proxy 192.168.31.5:9000";
       };
+      "staging.inclyc.cn" = {
+        serverAliases = [
+          "be.staging.inclyc.cn"
+          "admin.staging.inclyc.cn"
+          "applog.staging.inclyc.cn"
+        ];
+        extraConfig = "reverse_proxy 192.168.31.7:40000";
+      };
+      "production.inclyc.cn" = {
+        serverAliases = [
+          "be.production.inclyc.cn"
+          "admin.production.inclyc.cn"
+          "applog.production.inclyc.cn"
+        ];
+        extraConfig = "reverse_proxy 192.168.31.7:40001";
+      };
     };
   };
 }
